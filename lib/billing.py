0a490a316366ab26bf6b5b75b4441cfdc7b6e70e
Alexey Koloskov <koloskov@flexline.ru>
2014-06-19 18:39:17 +0400

    В описание полей статистики (_stat_fields) добавлен флаг сортировки. При удалении экземпляра закрываются все соединения с БД. При разборе конфига заполняется словарь self['dbconf'][<имя секции>] значениями соответствующей секции конфига. Функция _agent_id получает id агента, которому принадлежит ip, из БД, если в параметрах передан ip и он отличается от self['ip']. Функция _tables получает список таблиц из БД со статистикой, в которых содержатся занные в интервале timefrom - timeto. Начата разработка функции sum_stat (получение непосредственно статистики из БД со статистикой).

--- lib/billing.py
+++ lib/billing.py
@@ -33,21 +33,21 @@ class Dbi(UserDict, gwman):
                 }
 
     """Каждое запись словаря это структура:
-    'идентификатор поля': ('имя поля mysql', 'описание поля', включить_в_выборку?, группировать?),
+    'идентификатор поля': ('имя поля mysql', 'описание поля', включить_в_выборку?, группировать?, сортировать?),
     """
     _stat_fields = {
-        'timefrom': ("timefrom", "время начала сессии", True, True),
-        'timeto': ("timeto", "время окончания сессии", True, True),
-        'ip': ("ip", "локальный ip", True, True),
-        'ipport': ("ipport", "локальный порт", True, True),
-        'remote': ("remote", "удаленный ip", True, True),
-        'remport': ("remport", "удаленный порт", True, True),
-        'in': ("sum(cin) as in", "входящий, байт", True, True),
-        'out': ("sum(cout) as out", "исходящий, байт", True, True),
-        'vg_id': ("vg_id", "учетная запись", False, False),
-        'agrm_id': ("agrm_id", "договор", False, False),
-        'uid': ("uid", "абонент", False, False),
-        'tar_id': ("tar_id", "тариф", False, False),
+        'timefrom': ("timefrom", "время начала сессии", True, True, False),
+        'timeto': ("timeto", "время окончания сессии", True, True, False),
+        'ip': ("ip", "локальный ip", True, True, False),
+        'ipport': ("ipport", "локальный порт", True, True, False),
+        'remote': ("remote", "удаленный ip", True, True, False),
+        'remport': ("remport", "удаленный порт", True, True, False),
+        'in': ("sum(cin)", "входящий, байт", True, True, False),
+        'out': ("sum(cout)", "исходящий, байт", True, True, False),
+        'vg_id': ("vg_id", "учетная запись", False, False, False),
+        'agrm_id': ("agrm_id", "договор", False, False, False),
+        'uid': ("uid", "абонент", False, False, False),
+        'tar_id': ("tar_id", "тариф", False, False, False),
         }
 
 
@@ -66,7 +66,8 @@ class Dbi(UserDict, gwman):
         self.open_db('dbi')
 
     def __del__(self):
-        self.close_db('dbi')
+        for db in self.data._db_.keys():
+            self.close_db(db)
 
 
     @property
@@ -117,7 +118,13 @@ class Dbi(UserDict, gwman):
         config = ConfigParser.RawConfigParser()
         config.read(self.conf)
         if config.has_section(section):
-            return dict(config.items(section))
+            result = dict(config.items(section))
+
+        if not self.data.get('dbconf'):
+            self.data['dbconf'] = dict()
+
+        self.data['dbconf'][section] = result
+        return result
 
 
     def field_descr(self, field):
@@ -130,11 +137,87 @@ class Dbi(UserDict, gwman):
         """Возвращает id агента, на котором ip-адрес ip.
         если self['ip'] отличается от ip, то запрос в базу
         иначе вернет self['id']""" 
-        _ip = ip and self._check_ip(ip)
+        # Если параметр ip задан и проходит проверку, то _ip = ip,
+        # иначе _ip = self.data.get('ip') иначе ip = None
+        _ip = ip and self._check_ip(ip) or self.data.get('ip')
+        if not _ip:
+            raise ValueError("_agent_id(ip): ip or self['ip'] must be set!")
         if _ip == self.data.get('ip'):
             result = self.data.get('id')
         else:
-            #TODO: получение id агента из БД
+            sql = """
+                select
+                    s.id as id
+                from
+                    staff st inner join segments s
+                    on (st.segment_id = s.record_id)
+                where
+                    st.segment = inet_aton('{0}')
+                """.format(_ip)
+
+            cur = self._cur_['dbi']
+            cur.execute(sql)
+            if cur.rowcount == 0:
+                return False
+            elif cur.rowcount > 1:
+                raise RuntimeError("'{0}' belongs to many segments O_o... It's impossible!".format(ip))
+            elif cur.rowcount < 0:
+                raise RuntimeError("MySQLdb.cursor.rowcount return value < 0... Is it possible?")
+
+            # Here cur.rowcount = 1
+            row = cur.fetchone()
+            result = res['id']
+
+        return str(result)
+
+
+    def _tables(self, timefrom='19011213', timeto='20380119', agent=None):
+        """Возвращает отсортированный список таблиц.
+        Параметры: даты формата YYYYMMDD, включая С исключая ПО,
+        agent = id агента"""
+        _param_err = """params 'timefrom' and 'timeto' must be string representation of date YYYYMMDD
+                        param 'agent' must be string representation of integer with length <= 3"""
+        _agent = str(agent) or self._agent_id()
+        assert type(timefrom) == type(str()) and len(timefrom) == 8, _param_err
+        assert type(timeto) == type(str()) and len(timefrom) == 8, _param_err
+        assert timefrom.isdigit() and timeto.isdigit(), _param_err
+        assert _agent.isdigit() and len(timefrom) <= 3, _param_err
+        
+        # Получение имени БД со статистикой
+        _dbconf = self.data.get('dbconf')
+        assert type(_dbconf) == type(dict()), "self['dbconf'] must be a dictionary"
+        if not _dbconf.get('billstat'):
+            self.open_db('billstat')
+        db = _dbconf['billstat']['db']
+
+        # Format agent id as '00N'
+        _agent = "{:03d}".format(_agent)
+        prefix = "user{0}".format(_agent)
+        
+        sql = """
+                select
+                    t.TABLE_NAME as tbl
+                from
+                    information_schema.TABLES t
+                where
+                    t.TABLE_SCHEMA = '{db}'
+                    and t.TABLE_NAME like '{prefix}'
+                    and substr(TABLE_NAME,8) between '{dfrom}' and '{dto}'
+                order by
+                    t.TABLE_NAME
+              """.format(prefix = prefix, dfrom = timefrom, dto = str(int(timeto)-1), db = db)
+
+        cur = self._cur_['billstat']
+        cur.execute(sql)
+        if cur.rowcount == 0:
+            return False
+        #TODO: А эта проверка вообще нужна?
+        assert cur.rowcount >= 0, "MySQLdb.cursor.rowcount return value < 0... Is it possible?"
+
+        # Here cur.rowcount >= 1
+        rows = cur.fetchall()
+        result = [row.get('tbl') for row in rows]
+        return result
 
 
     def _ipinfo(self, params=None):
@@ -183,7 +266,7 @@ class Dbi(UserDict, gwman):
         elif cur.rowcount < 0:
             raise RuntimeError("MySQLdb.cursor.execute return value < 0... Is it possible?")
         
-        # Here self._cur_.rowcount = 1
+        # Here cur.rowcount = 1
         res = cur.fetchall()
         vg = res[0]
 
@@ -197,7 +280,7 @@ class Dbi(UserDict, gwman):
         return True
 
 
-    def sum_stat(self, timefrom='1901-12-13', timeto='2038-01-19'):
+    def sum_stat(self, timefrom='19011213', timeto='20380119', sort='timefrom'):
         """Параметры: условия отбора
         возвращает структуру:
         {'header':
@@ -213,6 +296,59 @@ class Dbi(UserDict, gwman):
         """
         if not self.ip:
             raise ValueError("Dbi.ip must be assigned first")
+
+        try:
+            self._stat_fields[sort][4] = True
+        except KeyError, e:
+            raise KeyError("key '{}' not found in self._stat_fields".format(sort))
+
+        self._stat_fields['timefrom'][0] = 'day(timefrom)'
+
+        # Формиррование параметров запроса
+        # Поля для выборки и группировка с сортировкой
+        fields = list()
+        group = list()
+        sort = list()
+        for k, v in self._stat_fields.items():
+            # Если поле не включать в отбор, то переходим к следующему
+            if not v[2]:
+                continue
+            # Если группировка или сортировка по полю, добавляем в соответствующий список
+            if v[3]:
+                group.append(k) 
+            if v[4]:
+                sort.append(k) 
+            fields.append("{0} as '{1}'".format(v[0], k))
+        cond = "ip = inet_aton('{ip}')".format(self.ip)
+        
+        params = {'fields': ", ".join(fields),
+                  'cond': cond,
+                  'group': ", ".join(group),
+                  'sort': ", ".join(sort),
+                 }
+
+        # Из каждой таблицы получаем статистику
+        data = list()
+        cur = self._cur_['billstat']
+        tables = self._tables(timefrom, timeto)
+        for table in tables:
+            params['table'] = table
+            sql = """
+                    select {fields}
+                    from {table}
+                    where {cond}
+                    group by {group}
+                    order by {order}
+                  """.format(**params)
+            cur.execute(sql)
+            for row in cur.fetchall():
+                data.expand(row.values())
+
+        result = {'header': list(),
+                  'body': data,
+                  'footer': list(),
+                  }
+
 
 
 def main():
